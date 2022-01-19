0a7c112c2fb913feb207ff5c94bda7491726f818
Ioana_Taran <Ioana_Taran@Dellteam.com>
2022-01-19 23:28:18 +0200

    Add tracklisting component. Fix Album interface structure

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,11 @@
 import { Track } from './track';
 
 export interface Album {
-  name: string;
-  releaseDate: string;
-  coverImage: string;
-  tracks: Track[];
+  artist: string;
+  album: {
+    name: string;
+    releaseDate: string;
+    coverImage: string;
+    tracks: Track[];
+  };
 }
